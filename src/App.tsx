f8cefb33b59bd1570da3a9847db929179bef40da
sudeep916 <sudeep731712@gmail.com>
2024-10-11 16:41:20 +0530

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import {Button} from '@mui/material'
-import Page1 from './components/page1/Page1';
-import SignIn from './components/signIn/SignIn';
+import Footer from './components/Footer/Footer';
+import { Box } from '@mui/material';
+// import {Button} from '@mui/material'
+// import Page1 from './components/page1/Page1';
+// import SignIn from './components/signIn/SignIn';
 import Navbar from './components/Header/Navbar';
 function App() {
   return (
@@ -11,6 +13,16 @@ function App() {
       <div>
         <Navbar/>
       </div>
+      <Box
+      sx={{
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <Box sx={{ flex: 1 }}> {/* Your main content goes here */} </Box>
+      <Footer />
+    </Box>
     </div>
   );
 }
